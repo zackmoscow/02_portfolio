5c5562651cd1071d3ec1a80873e3ad0ba1435873
Zack Moscow <zmoscow@gmail.com>
2020-02-09 11:57:21 -0600

    Updates

---
+++ precache-manifest.41d18b086d2ca7a94ccd545ccded75d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f62bb246a81d799595e493580b2675e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "993d774be0b9522463b6",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ea87bfb459ece2e6a48a",
+    "url": "/static/js/2.67a64ca4.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/static/js/2.67a64ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993d774be0b9522463b6",
+    "url": "/static/js/main.133ccfde.chunk.js"
+  },
+  {
+    "revision": "e3863f28f90bf2ec10cc",
+    "url": "/static/js/runtime-main.2a31b61e.js"
+  }
+]);
