e98c728d267725670a25e81145a0f8abd6d51d82
Zack Moscow <zmoscow@gmail.com>
2020-02-09 12:00:20 -0600

    Updates

---
+++ precache-manifest.76cdcd408b8efb042dcea28be6f12f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2774a1cd46205bbd1b61d6c73ac0dd5",
+    "url": "zackmoscow.github.io/index.html"
+  },
+  {
+    "revision": "a3a1b7a593cb86912ec0",
+    "url": "zackmoscow.github.io/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ea87bfb459ece2e6a48a",
+    "url": "zackmoscow.github.io/static/js/2.67a64ca4.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "zackmoscow.github.io/static/js/2.67a64ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3a1b7a593cb86912ec0",
+    "url": "zackmoscow.github.io/static/js/main.5cc4dfa3.chunk.js"
+  },
+  {
+    "revision": "c90586b71b729b1869d3",
+    "url": "zackmoscow.github.io/static/js/runtime-main.fe002e43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41d18b086d2ca7a94ccd545ccded75d1.js"
+  "zackmoscow.github.io/precache-manifest.76cdcd408b8efb042dcea28be6f12f49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("zackmoscow.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
